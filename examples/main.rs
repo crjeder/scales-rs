162729017a5110a6d953666c89d54075062cd21a
Christoph <crjeder@gmail.com>
2023-03-17 18:27:50 +0100

    compiles again

---
+++ examples/main.rs
@@ -0,0 +1,29 @@
+use embedded_hal::blocking::spi::{Transfer, Write};
+
+// embedded_hal implementation
+use embedded_hal_mock::spi::{Mock as Spi, Transaction as SpiTransaction};
+
+use hx711_spi::Hx711;
+use nb::block;
+
+// minimal example
+fn main() -> Result<(), Error> {
+    // Data the mocked up SPI bus shou
+    let expectations = [
+    SpiTransaction::send(0x09),
+    SpiTransaction::read(0x0A),
+    SpiTransaction::send(0xFE),
+    SpiTransaction::read(0xFF),
+    SpiTransaction::write(vec![1, 2]),
+    SpiTransaction::transfer(vec![3, 4], vec![5, 6]),
+];
+
+    let spi = Spi::new(&expectations)?;
+    let mut hx711 = Hx711::new(spi);
+
+    hx711.reset()?;
+    let v = block!(hx711.read())?;
+    println!("value = {}", v);
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,30 +2,35 @@
 #![no_std]
 
 use core::marker::PhantomData;
-//use core::{f64, i32, result::Result};
+use core::ops::{Sub, Div, Mul};
 
-trait Readable<E> {
-    fn read(&mut self) -> Result<i32, E>;
+trait Readable<Int, E> 
+    where
+    Int: Sub<Int>, 
+{
+    fn read(&mut self) -> Result<Int, E>;
 }
 
 #[derive(Debug, PartialEq)]
 /// Struct to hold the important values: tare, calibration factor and the device to 
 /// read the data from.
-struct Scales<T: Readable<E>, E> {
-    tare: i32,
-    calibration: f32,
+struct Scales<T: Readable<Int, E>, E, Int: Sub<Int> + From<Fp> + core::convert::From<<Int as core::ops::Sub>::Output>, Fp: Div<Fp> + From<Int>> {
+    tare: Int,
+    calibration: Fp,
     device: T,
     ph: PhantomData<E>,
 }
 
 #[allow(dead_code)]
-impl<T, E> Scales<T, E>
+impl<T, E, Int, Fp> Scales<T, E, Int, Fp>
 where
-    T: Readable<E>,
+    T: Readable<Int, E>,
+    Fp: Div<Fp> + From<Int> + Mul<Output = Fp> + Copy, 
+    Int: Sub<Int> + From<Fp> + core::convert::From<<Int as Sub>::Output> + Copy,
 {
     /// Create a new scales instance for a specific device and set the values for 
     /// tare and calibration
-    fn new(tare: i32, calibration: f32, device: T) -> Self {
+    fn new(tare: Int, calibration: Fp, device: T) -> Self {
         Self {
             tare,
             calibration,
@@ -35,14 +40,19 @@ where
     }
 
     /// Change the tare value to an new one.
-    fn tare(&mut self, tare: i32) {
+    fn tare(&mut self, tare: Int) {
         self.tare = tare;
     }
 
     /// Get a current reading from the scales device 
     /// (calibrated and tare'ed)
-    fn read(&mut self) -> Result<i32, E> {
-        let value = self.device.read()?;
-        Ok((f64::from(value - self.tare) * f64::from(self.calibration)) as i32)
+    /// # Errors
+    /// The read() errors of the underlaying device
+    fn read(&mut self) -> Result<Int, E> {
+        let value: Int = self.device.read()?;
+        //Ok(Int::from(Fp::from(value - self.tare) * self.calibration))
+        let tared: Int = (value - self.tare).into();
+        let calibrated: Fp = Fp::from(tared) * self.calibration;
+        Ok(Int::from(calibrated))
     }
 }
