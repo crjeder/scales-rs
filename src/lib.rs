51124d10b7e7f47ced4c776ae377b9bd4c3aee9c
Christoph <crjeder@gmail.com>
2024-04-29 16:03:56 +0200

    von vorne, nachdem ich den https://github.com/NeoBirth/accelerometer.rs/blob/develop/src/accelerometer.rs gesehen habe

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,20 @@
 #![forbid(unsafe_code)]
 #![no_std]
 
-use core::marker::PhantomData;
 use core::ops::{Sub, Div, Mul};
-
-trait Readable<Int, E> 
-    where
-    Int: Sub<Int>, 
-{
-    fn read(&mut self) -> Result<Int, E>;
-}
 
 #[derive(Debug, PartialEq)]
 /// Struct to hold the important values: tare, calibration factor and the device to 
 /// read the data from.
-struct Scales<T: Readable<Int, E>, E, Int: Sub<Int> + From<Fp> + core::convert::From<<Int as core::ops::Sub>::Output>, Fp: Div<Fp> + From<Int>> {
+struct ScalesData<Int: Sub<Int> + From<Fp> + core::convert::From<<Int as core::ops::Sub>::Output>, Fp: Div<Fp> + From<Int>> {
     tare: Int,
+    offset: Int,
     calibration: Fp,
-    device: T,
-    ph: PhantomData<E>,
 }
 
 #[allow(dead_code)]
-impl<T, E, Int, Fp> Scales<T, E, Int, Fp>
-where
-    T: Readable<Int, E>,
-    Fp: Div<Fp> + From<Int> + Mul<Output = Fp> + Copy, 
-    Int: Sub<Int> + From<Fp> + core::convert::From<<Int as Sub>::Output> + Copy,
-{
-    /// Create a new scales instance for a specific device and set the values for 
-    /// tare and calibration
-    fn new(tare: Int, calibration: Fp, device: T) -> Self {
-        Self {
-            tare,
-            calibration,
-            device,
-            ph: PhantomData,
-        }
-    }
-
+pub trait Scales {
+    type Error: Debug;
     /// Change the tare value to an new one.
     fn tare(&mut self, tare: Int) {
         self.tare = tare;
@@ -48,10 +24,11 @@ where
     /// (calibrated and tare'ed)
     /// # Errors
     /// The read() errors of the underlaying device
-    fn read(&mut self) -> Result<Int, E> {
+    fn read(&mut self, ScalesData data) -> Result<Int, Self::Error> {
         let value: Int = self.device.read()?;
+        // reading = (raw / calibration) - offset - tare
         //Ok(Int::from(Fp::from(value - self.tare) * self.calibration))
-        let tared: Int = (value - self.tare).into();
+        let tared: Int = (value - self.offset - self.tare).into();
         let calibrated: Fp = Fp::from(tared) * self.calibration;
         Ok(Int::from(calibrated))
     }
